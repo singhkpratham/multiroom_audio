0bbb1a8545fb4c0359ad51e927ad2809aa2e2ee6
singhkpratham <singhkpratham@gmail.com>
2022-07-14 00:23:21 +0530

    keep necessary dockerfiles, make changes to make mopidy run

--- mopidy/mopidy.sh
+++ mopidy/mopidy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Copy config if it does not already exist
-if [ ! -f /home/mopidy/.config/mopidy/mopidy.conf ]; then
-    cp /mopidy_default.conf /home/mopidy/.config/mopidy/mopidy.conf
-fi
+mkdir -p /home/mopidy/.config/mopidy/
+cp /mopidy_default.conf /home/mopidy/.config/mopidy/mopidy.conf
+
 
 if [ ${APT_PACKAGES:+x} ]; then
     echo "-- INSTALLING APT PACKAGES $APT_PACKAGES --"
@@ -21,4 +21,6 @@ if [ ${UPDATE:+x} ]; then
     pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U # Upgrade all pip packages
 fi
 
+mopidy --config /home/mopidy/.config/mopidy/mopidy.conf local scan
 exec mopidy --config /home/mopidy/.config/mopidy/mopidy.conf "$@"
+
